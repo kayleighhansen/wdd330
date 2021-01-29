75e3fa2696e8f43d4f5965e39a46f913018f2220
pardonmygiraffee <han16017@byui.edu>
2021-01-28 18:11:39 -0700

    code week 04

---
+++ week04/code/main.js
@@ -0,0 +1,49 @@
+//select all td tags and add a touchend event listener
+
+const squares = document.querySelectorAll('td');
+squares.forEach(
+    td => {
+        td.addEventListener("touchend", move);
+    } 
+)
+
+//select the player indicator at the bottom of the screen
+document.querySelector('.turn-indicator').innerHTML = "Player 1's Turn";
+
+//set player to default 0
+let player = 0;
+
+//changes the turn each time this function is called
+function next_turn() {
+    return player = player + 1;
+}
+
+//each time a box is touched
+function move(event, player) {
+    //change the turn of the player 
+    let turn = next_turn(player);
+    console.log(turn);
+    
+    //if the turn is even, place an X
+    if (turn % 2 == 0) {
+        your_square = event.target.className;
+        event.target.innerHTML = "X";
+
+        //change the indicator at the bottom of the screen
+        document.querySelector('.turn-indicator').innerHTML = "Player 1's Turn";
+    }
+    
+    //if it is odd, place a 0
+    else if (turn % 2 == 1) {
+        your_square = event.target.className;
+        event.target.innerHTML = "0";
+
+        //change the indicator at the bottom of the screen
+        document.querySelector('.turn-indicator').innerHTML = "Player 2's Turn";
+    }
+
+    //if there have been 9 turns, then the game is over
+    if (turn == 9) {
+        document.querySelector('.turn-indicator').innerHTML = "Game Over";
+    }
+}
