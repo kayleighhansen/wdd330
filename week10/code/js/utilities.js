4d401645f4bcae0d32438299c957874c4ec74a6d
pardonmygiraffee <han16017@byui.edu>
2021-03-11 22:06:09 -0700

    ...

---
+++ week10/code/js/utilities.js
@@ -0,0 +1,20 @@
+export async function getJSON(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        else {
+            //console.log(response.json());
+            return response.json();
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+  }
+  export const getLocation = function(options) {
+    return new Promise(function(resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+  };
