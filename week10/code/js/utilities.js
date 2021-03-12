6bffa2d1f22858baccb4e8825a58c61f1bfb2ca1
pardonmygiraffee <han16017@byui.edu>
2021-03-11 22:07:09 -0700

    ...

--- week10/code/js/utilities.js
+++ week10/code/js/utilities.js
@@ -5,7 +5,6 @@ export async function getJSON(url) {
             throw Error(response.statusText);
         }
         else {
-            //console.log(response.json());
             return response.json();
         }
     }
